b1cb3dceee1849535cadef93923de4794f60df1e
B Madhan Kumar <56861725+Madhan9498@users.noreply.github.com>
2023-08-23 06:37:13 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+import java.util.*;
+
+public class Hello{
+    public static void main(String[] args) {
+       
+        System.out.println("Hello 40");
+        System.out.println("50 World !");
+
+        
+    }
+}
